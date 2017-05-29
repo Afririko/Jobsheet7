83bf9554aaed5b34aa79ff1681941d60e450ddc7
Afririko <afririkosuprayatna07071996@gmail.com>
2017-05-29 15:52:33 +0700

    Add files via upload

---
+++ Latihan7.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Latihan Jobsheet 7</title>
+</head>
+<body>
+<form action="Biodata_post_latihan_7.php" method="post">
+<pre>
+Nama		: <input type="text" name="nama"  />
+
+Alamat		: <input type="text" name="alamat"  />
+
+Email		: <input type="text" name="email"  />
+
+Komentar	: <input type="text" name="komentar"  />
+
+		<input type="submit"/ name="kirim" value="Kirim">
+</pre>
+</form>
+</body>
+</html>
---
+++ Link1a.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Mengenal Manajemen Variabel</title>
+</head>
+<body>
+<?php
+	$kampusku="STEKOM SEMARANG";
+	echo"<a href=\"Link1b.php?kampusku=$kampusku\">Nama Kampusku </a>";
+?>
+</body>
+</html>
